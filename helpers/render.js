80ef7710f7b64142af3099fbeca825b45c076f40
mojo <igormalj@gmail.com>
2024-05-11 18:00:04 +0200

    refactor project from commonjs to es modules, organize public dir

--- helpers/render.js
+++
@@ -1,7 +0,0 @@
-const render = (response, content, contentType, statusCode) => {
-  response.writeHead(statusCode, { "content-type": contentType });
-  response.write(content);
-  response.end();
-};
-
-module.exports = render;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import url from "node:url";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const mainDirectory = "public";
 
 const getContentType = (extension) => {
   switch (extension) {
@@ -25,32 +26,47 @@ const getPublicSubDirectory = (extension) => {
   switch (extension) {
     case ".html":
       return "views";
+    case ".css":
+      return "css";
+    case ".js":
+      return "scripts";
+    case ".svg":
+      return "images";
+    default:
+      return "";
   }
 };
 
 const server = http.createServer(async (req, res) => {
   try {
     const fileExtension = path.extname(req.url) || ".html";
+    const subDirectory = getPublicSubDirectory(fileExtension);
+    const isIndex = req.url === "/";
+    const isMarkup = fileExtension === ".html";
 
     const filePath = path.join(
       __dirname,
-      "public",
-      req.url === "/"
-        ? "index.html"
-        : `${req.url}${fileExtension === ".html" ? fileExtension : ""}`
+      mainDirectory,
+      subDirectory,
+      isIndex ? "index.html" : `${req.url}${isMarkup ? fileExtension : ""}`
     );
 
     const data = await fs.readFile(filePath, { encoding: "utf-8" });
-    console.log(data);
-
     res.writeHead(200, { "Content-Type": getContentType(fileExtension) });
     res.write(data);
-    res.end();
+    return res.end();
   } catch (error) {
-    if (error.code === "ENOENT") {
-      res.write("error");
-      res.end();
-    }
+    // check if error is page not found or internal server error
+    const isNotFound = error.code === "ENOENT";
+    const errorMarkup = isNotFound ? "notFound.html" : "serverError.html";
+    const statusCode = isNotFound ? 404 : 500;
+
+    const data = await fs.readFile(
+      path.join(__dirname, mainDirectory, "views", errorMarkup)
+    );
+    res.writeHead(statusCode, { "Content-Type": getContentType(".html") });
+    res.write(data);
+    return res.end();
   }
 });
 
